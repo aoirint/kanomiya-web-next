2b6051c7aeb3990c71995c76fb8333c76b2cee0d
aoirint <aoirint@gmail.com>
2023-03-21 15:21:38 +0900

    add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar: React.FC<{}> = () => {
+  const [active, setActive] = React.useState<boolean>(false)
+
+  return (
+    <>
+      <section style={{ paddingTop: '3.25rem' }}>
+        <nav className="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Link className="navbar-item" href="/">
+              <Image src='/images/icon.png' alt="Logo image" width="28" height="28" />
+            </Link>
+
+            <button role="button" className={`navbar-burger ${active ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasic" onClick={() => {
+              setActive(!active)
+            }}>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </button>
+          </div>
+
+          <div id="navbarBasic" className={`navbar-menu ${active ? 'is-active' : ''}`}>
+            <div className="navbar-start">
+              <Link className="navbar-item" href="/">
+                ホーム
+              </Link>
+
+              <a className="navbar-item" rel="me" href="https://www.nicovideo.jp/user/91385177">
+                ニコニコ動画
+              </a>
+
+              <a className="navbar-item" rel="me" href="https://youtube.com/channel/UC7OazbQ3Eo9vrkcReXGIZkQ">
+                YouTube
+              </a>
+            </div>
+
+            <div className="navbar-end">
+              <a className="navbar-item" rel="me" href="https://twitter.com/kanomiyanic">
+                Twitter
+              </a>
+              <a className="navbar-item" rel="me" href="https://mstdn.aoirint.com/@kanomiyanic">
+                Fediverse
+              </a>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  )
+}
+
+export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Navbar from '@/components/Navbar'
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -8,14 +10,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="section">
-        <div className="container">
-          <h1 className="title">
-            Hello World
-          </h1>
-          <p className="subtitle">
-            My first website with <strong>Bulma</strong>!
-          </p>
+      <Navbar />
+      <section className='section'>
+        <div className='container'>
+          <div className='columns is-vcentered is-mobile'>
+            <div className='column is-narrow'>
+              <Image
+                src="/images/icon.png"
+                alt="Logo image"
+                className='image is-96x96'
+                width="96"
+                height="96"
+              />
+            </div>
+            <div className='column'>
+              <h1 className='title is-2'>
+                かのみや
+              </h1>
+              <p className='subtitle is-6 pt-1'>
+                ニコニコ・ボイロ・ゲーム
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
